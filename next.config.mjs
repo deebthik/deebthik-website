a7e9b4ed31d0e2eb1e30ffa071de3b91fe010c49
Deebthik <deebthik@deebthiks-mbp.dhcp.asu.edu>
2024-11-04 16:02:13 -0700

    Binseg paper folder renamed and URL redirect for google scholar

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,15 @@ const nextConfig = {
 	experimental: {
 		mdxRs: true,
 	},
+	async redirects() {
+		return [
+			{
+				source: '/vocation/publications-preprints/BinSeg-Leveraging_Semantic_Segmentation_for_Code_and_Data_Separation_in_Non-Standard_Binary_Formats__DK.pdf',
+				destination: '/papers/BinSeg-Leveraging_Semantic_Segmentation_for_Code_and_Data_Separation_in_Non-Standard_Binary_Formats.pdf',
+				permanent: true,
+			},
+		];
+	},
 };
 
 export default withContentlayer(nextConfig);
